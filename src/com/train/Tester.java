1174e9ab4e97c9ae297bb3d398acbf48fc224a88
hayashi0928 <hayashi0928@gmail.com>
2019-08-07 23:21:05 +0800

    homework2

--- src/com/train/Tester.java
+++ src/com/train/Tester.java
@@ -1,12 +1,72 @@
 package com.train;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
-        Ticket tk=new Ticket();
-        tk.order();
-    }
+        int numberOfTicket=0;
+        int numberOfroundtrip=0;
+        List<Ticket> ticketOrder=new ArrayList<>();
+        Scanner scanner;
+        System.out.println("Please enter number of tickets(enter \"-1\" to end):");
+        //輸入訂票數
+        while(true) {
+            while (true) {
+                scanner = new Scanner(System.in);
+                if (scanner.hasNextInt()) {
+                    numberOfTicket = scanner.nextInt();
+                    if ((numberOfTicket >0)||(numberOfTicket==-1)) {
+                        break;
+                    } else {
+                        System.out.println("The number of tickets shall be at least one! Please enter number of tickets:");
+                    }
+                } else {
+                    System.out.println("Invalid number! Please enter number of tickets:");
+                }
+            }
+            //輸入來回票數
+            while (numberOfTicket>0) {
+                System.out.println("How many round-trip tickets: ");
+                scanner = new Scanner(System.in);
+                if (scanner.hasNextInt()) {
+                    numberOfroundtrip = scanner.nextInt();
+                    if (numberOfroundtrip  >= 0) {
+                        if (numberOfroundtrip > numberOfTicket) {
+                            System.out.println("Round-trip tickets can't be more than total tickets!");
+                        } else {
+                            break;
+                        }
+                    } else {
+                        System.out.println("The number of tickets shall be above zero!");
+                    }
+                } else {
+                    System.out.println("Invalid number!");
+                }
+            }
+            if(numberOfTicket==-1){
+                break;
+            }else{
+                ticketOrder.add(new Ticket(numberOfTicket,numberOfroundtrip));
+                System.out.println("\nSuccessful Booking! \nPlease enter number of tickets for next booking(enter \"-1\" to end):");
+            }
+        }
 
+        //印出訂票結果
+        if(ticketOrder.size()==0){
+            System.out.println("Ending without any booking.");
+        }else{
+            int totalPrice=0;
+            System.out.println("\nYour Bookings:");
+            for(Ticket tk:ticketOrder){
+                totalPrice+=tk.getPrice();
+                System.out.println(tk);
+            }
+            System.out.println("Total Price: " + totalPrice);
+        }
+    }
 
 }
 
--- src/com/train/TesterKotlin.kt
+++ src/com/train/TesterKotlin.kt
@@ -1,6 +1,59 @@
 package com.train
 
+import java.lang.NumberFormatException
+
 fun main(){
-    val tk=TicketKotlin()
-    tk.order()
+    var numberOfTicket=0
+    var numberOfRoundtrip=0
+    val ticketBooking:MutableList<TicketKotlin> = ArrayList()
+    println("Please enter number of tickets(enter \"-1\" to end):")
+    booking@ while(true) {
+        bookTicket@ while (true) {
+            //輸入訂票數
+            try {
+                numberOfTicket = Integer.parseInt(readLine())
+                if (numberOfTicket > 0 ) {
+                    break@bookTicket
+                }else if(numberOfTicket == -1){
+                    break@booking
+                }
+                else {
+                    println("The number of tickets shall be at least one! Please enter number of tickets:")
+                }
+            } catch (ex: NumberFormatException) {
+                println("Invalid number! Please enter number of tickets:")
+            }
+        }
+        //輸入來回票數
+        bookRoundtrip@ while (numberOfTicket > 0) {
+            println("How many round-trip tickets: ")
+            try {
+                numberOfRoundtrip = Integer.parseInt(readLine())
+                if (numberOfRoundtrip >= 0) {
+                    if (numberOfRoundtrip > numberOfTicket) {
+                        println("Round-trip tickets can't be more than total tickets! Please enter how many round-trip tickets:")
+                    } else {
+                        break@bookRoundtrip
+                    }
+                } else {
+                    println("The number of tickets shall be above zero! Please enter how many round-trip tickets:")
+                }
+            } catch (ex: NumberFormatException) {
+                println("Invalid number! Please enter how many round-trip tickets:")
+            }
+        }
+        ticketBooking.add(TicketKotlin(numberOfTicket,numberOfRoundtrip))
+        println("\nSuccessful Booking! \nPlease enter number of tickets for next booking(enter \"-1\" to end):")
+    }
+    if (ticketBooking.size==0){
+        print("Ending without any booking.")
+    }else{
+        var totalPrice=0
+        println("\nYour Bookings:")
+        for(tk:TicketKotlin in ticketBooking){
+            totalPrice += tk.price
+            println(tk)
+        }
+        println("Total Price: $totalPrice")
+    }
 }
